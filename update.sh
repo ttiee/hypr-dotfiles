54b8daa1af8e621ba46939bccc41b6138384b805
ttiee <469784630@qq.com>
2024-01-05 14:57:35 +0800

    Add ranger config

--- update.sh
+++ update.sh
@@ -1,10 +1,17 @@
 
 rm -r ./hypr
 cp -r ~/.config/hypr/ ./
+echo "update hypr"
 rm -r ./tofi
 cp -r ~/.config/tofi/ ./
+echo "update tofi"
 rm -r ./waybar
 cp -r ~/.config/waybar/ ./
+echo "update waybar"
 rm -r ./kitty
 cp -r ~/.config/kitty/ ./
+echo "update kitty"
+rm -r ./ranger
+cp -r ~/.config/ranger/ ./
+echo "update ranger"
 
